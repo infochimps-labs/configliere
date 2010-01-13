4391109c51ab36757abbc84494972b0d565fd062
Philip (flip) Kromer <flip@infochimps.org>
2010-01-13 12:59:52 -0600

    Much documentation to config_block

--- lib/configliere/config_block.rb
+++ lib/configliere/config_block.rb
@@ -1,25 +1,36 @@
 Configliere.use :define
 module Configliere
+  #
+  # ConfigBlock lets you use pure ruby to change and define settings.  Call
+  # +#finally+ with a block of code to be run after all other settings are in
+  # place.
+  #
+  #     Settings.finally{|c| c.your_mom[:college] = 'went' unless (! c.mom_jokes_allowed) }
+  #
   module ConfigBlock
-    # Config blocks to be executed at end of resolution (just before validation)
-    attr_accessor :final_blocks
-    def final_blocks
-      @final_blocks ||= []
-    end
-
-    # @param param the setting to describe. Either a simple symbol or a dotted param string.
-    # @param definitions the defineables to set (:description, :type, :encrypted, etc.)
+    #
+    # @param &block each +finally+ block is called once, in the order it was
+    #   defined, when the resolve! method is invoked. +config_block+ resolution
+    #   is guaranteed to run last in the resolve chain, right before validation.
     #
     # @example
-    #   Settings.define :dest_time, :type => Date, :description => 'Arrival time. If only a date is given, the current time of day on that date is assumed.'
-    #   Settings.define 'delorean.power_source', :description => 'Delorean subsytem supplying power to the Flux Capacitor.'
-    #   Settings.define :password, :required => true, :obscure => true
+    #   Settings.finally do |c|
+    #     c.dest_time = (Time.now + 60) if c.username == 'einstein'
+    #     # you can use hash syntax too
+    #     c[:dest_time] = (Time.now + 60) if c[:username] == 'einstein'
+    #   end
+    #   # ...
+    #   # after rest of setup:
+    #   Settings.resolve!
     #
     def finally &block
       self.final_blocks << block
     end
 
-    # calls superclass resolution
+    # Processing to reconcile all options
+    #
+    # The resolve! for config_block is made to run last of all in the +resolve!+
+    # chain, and runs each +finally+ block in the order it was defined.
     def resolve!
       begin ; super() ; rescue NoMethodError ; nil ; end
       resolve_finally_blocks!
@@ -27,12 +38,17 @@ module Configliere
     end
 
   protected
+    # Config blocks to be executed at end of resolution (just before validation)
+    attr_accessor :final_blocks
+    def final_blocks
+      @final_blocks ||= []
+    end
+    # call each +finally+ config block in the order it was defined
     def resolve_finally_blocks!
       final_blocks.each do |block|
         block.call(self)
       end
     end
-
   end
 
   Param.class_eval do
