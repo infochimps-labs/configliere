2d9b0b9d5a01339c3089d9359e44d54d083c5e60
Philip (flip) Kromer <flip@infochimps.org>
2010-01-15 01:54:42 -0600

    making example scripts match new interface

--- examples/config_block.rb
+++ examples/config_block.rb
@@ -3,9 +3,9 @@ require 'configliere'
 
 Settings :passenger => 'einstein', :dest_time => '1955-11-05', 'delorean.power_source' => :plutonium
 Settings.finally do |c|
-  p [self, 'finally', passenger, c, c[:passenger]]
+  p [self, 'finally', c[:passenger], c.passenger]
   # Einstein the dog should only be sent one minute into the future.
-  dest_time = (Time.now + 60) if passenger == 'einstein'
+  dest_time = (Time.now + 60) if c.passenger == 'einstein'
 end
 Settings.resolve!
 p Settings
--- examples/encrypted_script.rb
+++ examples/encrypted_script.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/env ruby
 require 'configliere'
 
+DUMP_FILENAME = '/tmp/encrypted_script.yml'
 Settings.use :config_file, :define, :encrypt_pass => 'password1'
-Settings.define 'password', :required => true, :encrypted => true
-Settings 'password' => 'plaintext'
-p Settings
-Settings.resolve!
-p Settings
-p Settings.send(:export)
-Settings.save!('file.yml')
+Settings.define :password, :encrypted => true
 
-Settings.password = 'before read'
-Settings.read('file.yml')
-Settings.password = 'before resolve'
+Settings :password => 'plaintext'
 Settings.resolve!
-p Settings
+p ["saved version will have encrypted password (see #{DUMP_FILENAME}).", Settings.send(:export)]
+p ['live version still has password in plaintext', Settings]
+Settings.save!(DUMP_FILENAME)
+
+Settings[:password] = 'before read'
+Settings.read('/tmp/encrypted_script.yml')
+Settings.resolve!
+p ["value is decrypted on resolve.", Settings]
