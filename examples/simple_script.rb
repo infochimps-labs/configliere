79a12272ce7f80ae9621184f0976becdc14e3e84
Philip (flip) Kromer <flip@infochimps.org>
2010-01-20 04:48:45 -0600

    uniform examples in scripts and readme

--- examples/simple_script.rb
+++ examples/simple_script.rb
@@ -3,21 +3,28 @@ $: << File.dirname(__FILE__)+'/../lib'
 require 'configliere'
 
 puts "This is a demo of Configliere in a simple script."
-Settings.use :commandline, :config_file
+Settings.use :commandline, :config_file, :define
 
 puts "You can set default values:"
-Settings.defaults :cat => 'bag', :cow => 'moon'
+Settings({
+  :dest_time => '1955-11-05',
+  :delorean => {
+    :power_source => 'plutonium',
+    :roads_needed => true,
+    },
+})
 puts "  #{Settings.inspect}"
 
 config_filename = File.dirname(__FILE__)+'/simple_script.yaml'
 puts "\nYou can load values from a file -- in this case, #{config_filename} -- which overrides the defaults:"
 Settings.read config_filename
+Settings.resolve!
 puts "  #{Settings.inspect}"
 
 puts %Q{\nTry running the script with commandline parameters, for example
-  #{$0} --sprats.wife=fat --spider=waterspout
+  #{$0} --dest_time=2015-11-05 --delorean.roads_needed="" --delorean.power_source="Mr. Fusion"
 In this case, you used
-  #{$0} #{ARGV.join(" ")}
+  #{$0} #{ARGV.map{|argv| "'#{argv}'"}.join(" ")}
 and so the final parameter values are}
 Settings.resolve!
 puts "  #{Settings.inspect}"
