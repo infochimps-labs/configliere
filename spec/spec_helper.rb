7cff771a7a203dfa7d4cc5b6ee802cf1f302ff56
zhon <zhon@xputah.org>
2011-05-12 15:41:14 -0600

    Fixed help for single-letter commands

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'configliere'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
+require 'rspec/autorun'
 
 Spec::Runner.configure do |config|
 
