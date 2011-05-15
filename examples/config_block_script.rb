ab58e1de5a3541620dd99e5186045e6a622740b9
Philip (flip) Kromer <flip@infochimps.org>
2011-05-15 06:02:31 -0500

    improved the config_block example

--- examples/config_block_script.rb
+++ examples/config_block_script.rb
@@ -8,13 +8,16 @@ Settings.define :dest_time, :type => DateTime
 Settings :passenger => 'einstein', :dest_time => '1955-11-05'
 
 Settings.finally do |c|
-  p ['(second) takes the settings object as arg', self, c[:passenger], c.passenger]
+  p ['(2) takes the settings object as arg', self, c[:passenger], c.passenger]
   # Einstein the dog should only be sent one minute into the future.
   c.dest_time = (Time.now + 60) if c.passenger == 'einstein'
 end
 
-Settings.finally{ p ['(third), because blocks go in order'] }
+Settings.finally{ p ['(3) note that blocks go in order'] }
 
-p ["(first) :finally blocks are called when you invoke resolve!"]
+Settings.define :mc_fly, :default => 'wuss',
+  :finally => lambda{ p ['(4) here is a block in the define'] ; Settings.mc_fly = 'badass' }
+
+p ["(1) :finally blocks are called when you invoke resolve!"]
 Settings.resolve!
-p ['(last) here are the settings', Settings]
+p ['(5) here are the settings', Settings]
