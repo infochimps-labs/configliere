9b3a7269f7ee01d2a37f2c234d25f57ffd9a1cab
Philip (flip) Kromer <flip@infochimps.org>
2011-05-15 22:04:34 -0500

    Prompt is now its own middleware

--- lib/configliere.rb
+++ lib/configliere.rb
@@ -1,16 +1,12 @@
+require 'yaml'
+require 'fileutils'
 require 'configliere/deep_hash'
 require 'configliere/param'
 require 'configliere/define'
 require 'configliere/config_file'
 
 module Configliere
-
-  # delegates to Configliere::Param
-  def self.new *args, &block
-    Configliere::Param.new(*args, &block)
-  end
-
-  ALL_MIXINS = [:define, :config_file, :commandline, :encrypted, :env_var, :config_block, :commands]
+  ALL_MIXINS = [:define, :config_file, :commandline, :encrypted, :env_var, :config_block, :commands, :prompt]
   def self.use *mixins
     mixins = ALL_MIXINS if mixins.include?(:all) || mixins.empty?
     mixins.each do |mixin|
@@ -18,13 +14,14 @@ module Configliere
     end
   end
 
-  # Base class for Configliere errors.
-  Error = Class.new(StandardError)
-
 end
 
+# Base class for Configliere errors.
+class Configliere::Error           < StandardError      ; end
+class Configliere::DeprecatedError < Configliere::Error ; end
+
 # Defines a global config object
-Settings = Configliere.new unless defined?(Settings)
+Settings = Configliere::Param.new unless defined?(Settings)
 
 #
 # Also define Settings as a function, so you can say
