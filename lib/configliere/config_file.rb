8badcec631f9658ea8b2f90af55bd15a455da9b4
Philip (flip) Kromer <flip@infochimps.org>
2011-05-15 22:32:36 -0500

    config_file now just takes a filename, no more magic handle. Instead, scope a segment of the file by supplying :env => :development (or whatever).

--- lib/configliere/config_file.rb
+++ lib/configliere/config_file.rb
@@ -1,8 +1,4 @@
-require 'yaml'
-require 'fileutils'
 module Configliere
-  # Where to load params given only a symbol
-  DEFAULT_CONFIG_FILE = ENV['HOME'].to_s+'/.configliere.yaml' unless defined?(DEFAULT_CONFIG_FILE)
   # Where to load params given a bare filename
   DEFAULT_CONFIG_DIR  = ENV['HOME'].to_s+'/.configliere'      unless defined?(DEFAULT_CONFIG_DIR)
 
@@ -10,29 +6,34 @@ module Configliere
   # ConfigFile -- load configuration from a simple YAML file
   #
   module ConfigFile
-    # Load params from disk.
-    # * file is in YAML format, as a hash of handle => param_hash pairs
-    # * filename defaults to Configliere::DEFAULT_CONFIG_FILE (~/.configliere, probably)
+    # Load params from a YAML file, as a hash of handle => param_hash pairs
     #
-    # @option [String] :env
+    # @param filename [String] the file to read. If it does not contain a '/',
+    #   the filename is expanded relative to Configliere::DEFAULT_CONFIG_DIR
+    # @param options [Hash]
+    # @option options :env [String]
     #   If an :env option is given, only the indicated subhash is merged. This
     #   lets you for example specify production / environment / test settings
     #
     # @returns [Configliere::Params] the Settings object
     #
     # @example
-    #     # Read from config/apey_eye.yaml and use settings appropriate for development/staging/production
-    #     Settings.read(root_path('config/foo.yaml'), :env => ENV['RACK_ENV'])
+    #     # Read from ~/.configliere/foo.yaml
+    #     Settings.read(foo.yaml)
     #
-    def read handle, options={}
-      filename = filename_for_handle(handle)
+    # @example
+    #     # Read from config/foo.yaml and use settings appropriate for development/staging/production
+    #     Settings.read(App.root.join('config', 'environment.yaml'), :env => ENV['RACK_ENV'])
+    #
+    def read filename, options={}
+      if filename.is_a?(Symbol) then raise Configliere::DeprecatedError, "Loading from a default config file is no longer provided" ; end
+      filename = expand_filename(filename)
       begin
         params = YAML.load(File.open(filename)) || {}
       rescue Errno::ENOENT => e
         warn "Loading empty configliere settings file #{filename}"
         params = {}
       end
-      params = params[handle] if handle.is_a?(Symbol)
       # Extract the :env (production/development/etc)
       if options[:env]
         params = params[options[:env]] || {}
@@ -44,44 +45,26 @@ module Configliere
     # save to disk.
     # * file is in YAML format, as a hash of handle => param_hash pairs
     # * filename defaults to Configliere::DEFAULT_CONFIG_FILE (~/.configliere, probably)
-    def save! handle
-      filename = filename_for_handle(handle)
-      if handle.is_a?(Symbol)
-        ConfigFile.merge_into_yaml_file filename, handle, self.export.to_hash
-      else
-        ConfigFile.write_yaml_file filename, self.export.to_hash
-      end
-    end
-
-  protected
-
-    def self.write_yaml_file filename, hsh
+    def save! filename
+      filename = expand_filename(filename)
+      hsh = self.export.to_hash
       FileUtils.mkdir_p(File.dirname(filename))
       File.open(filename, 'w'){|f| f << YAML.dump(hsh) }
     end
 
-    def self.merge_into_yaml_file filename, handle, params
-      begin
-        all_settings = YAML.load(File.open(filename)) || {}
-      rescue Errno::ENOENT => e;
-        all_settings = {}
-      end
-      all_settings[handle] = params
-      write_yaml_file filename, all_settings
-    end
+  protected
 
-    def filename_for_handle handle
-      case
-      when handle.is_a?(Symbol)      then Configliere::DEFAULT_CONFIG_FILE
-      when handle.to_s.include?('/') then File.expand_path(handle)
-      else                                File.join(Configliere::DEFAULT_CONFIG_DIR, handle)
+    def expand_filename filename
+      if filename.to_s.include?('/')
+        File.expand_path(filename)
+      else
+        File.expand_path(File.join(Configliere::DEFAULT_CONFIG_DIR, filename))
       end
     end
-
   end
 
+  # ConfigFile is included by default
   Param.class_eval do
-    # include read / save operations
     include ConfigFile
   end
 end
--- spec/configliere/config_file_spec.rb
+++ spec/configliere/config_file_spec.rb
@@ -1,91 +1,81 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-Configliere.use :config_file
 
-describe "Configliere::ConfigFile" do
+describe Configliere::ConfigFile do
   before do
-    @config = Configliere.new :my_param => 'val'
+    @config = Configliere::Param.new :my_param => 'default_val', :also_a_param => true
     FileUtils.stub! :mkdir_p
   end
 
-  describe 'loads a config file' do
+  it 'is used by default' do
+    @config.should respond_to(:read)
+  end
+
+  describe 'loading a config file' do
     before do
-      @config = Configliere::Param.new :my_param => 'val'
-      @fake_file = '{ :my_param: val }'
+      @fake_file = '{ :my_param: val_from_file }'
     end
+
     describe 'successfully' do
-      it 'loads a symbol name from the default config file' do
-        File.should_receive(:open).with(Configliere::DEFAULT_CONFIG_FILE).and_return(':my_settings: ' + @fake_file)
-        @config.read :my_settings
-      end
-      it 'loads an absolute pathname from the given file' do
+      it 'with an absolute pathname uses it directly' do
         File.should_receive(:open).with('/fake/path.yaml').and_return(@fake_file)
         @config.read '/fake/path.yaml'
       end
-      it 'loads a simple filename from the default config dir' do
+      it 'with a simple filename, references it to the default config dir' do
         File.should_receive(:open).with(Configliere::DEFAULT_CONFIG_DIR + '/file.yaml').and_return(@fake_file)
         @config.read 'file.yaml'
       end
-      it 'loads an absolute pathname from the given file' do
-        File.should_receive(:open).with('/fake/path.yaml').and_return(@fake_file)
-        @config.read '/fake/path.yaml'
+      it 'returns the config object for chaining' do
+        File.should_receive(:open).with(Configliere::DEFAULT_CONFIG_DIR + '/file.yaml').and_return(@fake_file)
+        @config.defaults :also_a_param => true
+        @config.read('file.yaml').should == { :my_param => 'val_from_file', :also_a_param => true }
       end
       after do
-        @config[:my_param].should == 'val'
+        @config[:my_param].should == 'val_from_file'
       end
     end
-    describe 'in edge cases' do
-      it 'handles a file not found' do
-        @config = Configliere::Param.new
-        File.stub(:open).and_raise(Errno::ENOENT)
-        @config.should_receive(:warn).with("Loading empty configliere settings file #{DEFAULT_CONFIG_DIR}/nonexistent_file.yaml")
-        @config.read('nonexistent_file.yaml').should == {}
-      end
+
+    it 'no longer provides a default config file' do
+      lambda{ @config.read(:my_settings) }.should raise_error(Configliere::DeprecatedError)
+      defined?(Configliere::DEFAULT_CONFIG_FILE).should_not be_true
+    end
+
+    it 'warns but does not fail if the file is missing' do
+      @config = Configliere::Param.new
+      File.stub(:open).and_raise(Errno::ENOENT)
+      @config.should_receive(:warn).with("Loading empty configliere settings file #{Configliere::DEFAULT_CONFIG_DIR}/nonexistent_file.yaml")
+      @config.read('nonexistent_file.yaml').should == {}
     end
   end
 
   describe 'saves to a config file' do
-    describe 'successfully' do
-      it 'saves a symbol name to the default config file' do
-        Configliere::ConfigFile.should_receive(:merge_into_yaml_file).
-          with(Configliere::DEFAULT_CONFIG_FILE, :my_settings, { :my_param => 'val'})
-        @config.save! :my_settings
-      end
-      it 'saves a pathname to the given file' do
-        fake_file = StringIO.new('', 'w')
-        File.should_receive(:open).with('/fake/path.yaml', 'w').and_yield(fake_file)
-        fake_file.should_receive(:<<).with("--- \n:my_param: val\n")
-        @config.save! '/fake/path.yaml'
-      end
-      it 'ensures the directory exists' do
-        fake_file = StringIO.new('', 'w')
-        File.stub!(:open).with('/fake/path.yaml', 'w').and_yield(fake_file)
-        FileUtils.should_receive(:mkdir_p).with('/fake')
-        @config.save! '/fake/path.yaml'
-      end
-    end
-  end
-
-  describe 'merge_into_yaml_file' do
-    it 'merges, leaving existing values put' do
-      fake_file = StringIO.new(":my_settings: { :my_param: orig_val }\n:other_settings: { :that_param: other_val }", 'r+')
-      File.should_receive(:open).with('/fake/path.yaml').and_return(fake_file)
+    it 'with an absolute pathname, as given' do
+      fake_file = StringIO.new('', 'w')
       File.should_receive(:open).with('/fake/path.yaml', 'w').and_yield(fake_file)
-      mock_dump = 'mock_dump'
-      YAML.should_receive(:dump).with({ :my_settings => { :my_param => 'new_val'}, :other_settings => { :that_param => 'other_val'}}).and_return(mock_dump)
-      fake_file.should_receive(:<<).with(mock_dump)
-      Configliere::ConfigFile.merge_into_yaml_file '/fake/path.yaml', :my_settings, :my_param => 'new_val'
+      fake_file.should_receive(:<<).with("--- \n:my_param: default_val\n:also_a_param: true\n")
+      @config.save! '/fake/path.yaml'
+    end
+
+    it 'with a simple pathname, in the default config dir' do
+      fake_file = StringIO.new('', 'w')
+      File.should_receive(:open).with(Configliere::DEFAULT_CONFIG_DIR + '/file.yaml', 'w').and_yield(fake_file)
+      fake_file.should_receive(:<<).with("--- \n:my_param: default_val\n:also_a_param: true\n")
+      @config.save! 'file.yaml'
+    end
+
+    it 'and ensures the directory exists' do
+      fake_file = StringIO.new('', 'w')
+      File.stub!(:open).with('/fake/path.yaml', 'w').and_yield(fake_file)
+      FileUtils.should_receive(:mkdir_p).with('/fake')
+      @config.save! '/fake/path.yaml'
     end
   end
 
-  describe 'maps handles to filenames' do
-    it 'loads a symbol name from the default config file' do
-      @config.send(:filename_for_handle, :my_settings).should == Configliere::DEFAULT_CONFIG_FILE
-    end
-    it 'loads an absolute pathname from the given file' do
-      @config.send(:filename_for_handle, '/fake/path.yaml').should == '/fake/path.yaml'
-    end
-    it 'loads a simple filename from the default config dir' do
-      @config.send(:filename_for_handle, 'file.yaml').should == Configliere::DEFAULT_CONFIG_DIR + '/file.yaml'
+  describe '#resolve!' do
+    it 'calls super and returns self' do
+      Configliere::ParamParent.class_eval do def resolve!() dummy ; end ; end
+      @config.should_receive(:dummy)
+      @config.resolve!.should equal(@config)
+      Configliere::ParamParent.class_eval do def resolve!() self ; end ; end
     end
   end
 
