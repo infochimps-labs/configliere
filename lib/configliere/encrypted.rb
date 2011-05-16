176b6ba38d9e5e7b742412bde330cb27c9a99454
Philip (flip) Kromer <flip@infochimps.org>
2011-05-15 22:39:56 -0500

    cleaned up handling of encrypt_pass -- it's no longer publicly readable; set it as a member (Settings[:encrypt_pass]) or through the ENCRYPT_PASS environment variable and it will be adopted in the course of action (and, if a member, deleted). Because of the #use method refactoring, you can have independent settings bundles use encrypted independently.

--- lib/configliere/encrypted.rb
+++ lib/configliere/encrypted.rb
@@ -1,9 +1,6 @@
-Configliere.use :config_file, :define, :crypter
-
+require 'configliere/crypter'
 module Configliere
   module EncryptedParam
-    # The password used in encrypting params during serialization
-    attr_accessor :encrypt_pass
 
     # decrypts any encrypted params
     # then calls the next step in the resolve! chain.
@@ -41,7 +38,8 @@ module Configliere
 
     # if :encrypted_pass was set as a param, remove it from the hash and set it as an attribute
     def remove_and_adopt_encrypt_pass_param_if_any!
-      @encrypt_pass = self.delete(:encrypt_pass) if self[:encrypt_pass]
+      @encrypt_pass ||= self.delete(:encrypt_pass) if self[:encrypt_pass]
+      @encrypt_pass ||= ENV['ENCRYPT_PASS']        if ENV['ENCRYPT_PASS']
     end
 
     # the chain of symbol keys for a dotted path key,
@@ -51,28 +49,29 @@ module Configliere
     #    encrypted_key_path('amazon.api.key')
     #    #=> [:amazon, :api, :encrypted_key]
     def encrypted_key_path param
-      encrypted_path = convert_key(param).dup
+      encrypted_path = Array(convert_key(param))
       encrypted_path[-1] = "encrypted_#{encrypted_path.last}".to_sym
       encrypted_path
     end
 
     # list of all params to encrypt on serialization
     def encrypted_params
-      params_with(:encrypted)
+      params_with(:encrypted).keys.select{|p| definition_of(p, :encrypted) }
     end
 
     def decrypted val
       return val.to_s if val.to_s.empty?
-      Configliere::Crypter.decrypt(val, encrypt_pass)
+      Configliere::Crypter.decrypt(val, @encrypt_pass)
     end
 
     def encrypted val
       return unless val
-      Configliere::Crypter.encrypt(val, encrypt_pass)
+      Configliere::Crypter.encrypt(val, @encrypt_pass)
     end
   end
 
-  class Param
-    include EncryptedParam
+  Param.on_use(:encrypted) do
+    use :config_file, :define
+    extend EncryptedParam
   end
 end
--- spec/configliere/encrypted_spec.rb
+++ spec/configliere/encrypted_spec.rb
@@ -1,45 +1,59 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-Configliere.use :encrypted
 
 describe "Configliere::Encrypted" do
   before do
     @config = Configliere::Param.new :secret => 'encrypt_me', :normal_param => 'normal'
+    @config.use :encrypted
     @config.define :secret, :encrypted => true
-    @config.encrypt_pass = 'pass'
+    @config[:encrypt_pass] = 'pass'
   end
 
-  describe 'defining encrypted params' do
-    it 'is encrypted if defined with :encrypted => true' do
+  describe 'defines encrypted params' do
+    it 'with :encrypted => true' do
       @config.send(:encrypted_params).should include(:secret)
     end
-    it 'is not encrypted if defined with :encrypted => false' do
+    it 'but not if :encrypted => false' do
       @config.define :another_param,   :encrypted => false
       @config.send(:encrypted_params).should_not include(:another_param)
       @config.send(:encrypted_params).should     include(:secret)
     end
-    it 'is encrypted if not defined' do
+    it 'only if :encrypted is given' do
       @config.send(:encrypted_params).should_not include(:missing_param)
     end
   end
 
-  describe 'encrypting encryptable params' do
-    it 'encrypts all params marked encrypted' do
+  describe 'the encrypt_pass' do
+    it 'will take an environment variable if any exists' do
+      @config[:encrypt_pass] = nil
+      ENV.should_receive(:[]).with('ENCRYPT_PASS').at_least(:once).and_return('monkey')
+      @config.send(:export)
+      @config.send(:instance_variable_get, "@encrypt_pass").should == 'monkey'
+    end
+    it 'will take an internal value if given, and remove it' do
+      @config[:encrypt_pass] = 'hello'
+      @config.send(:export)
+      @config.send(:instance_variable_get, "@encrypt_pass").should == 'hello'
+      @config[:encrypt_pass].should be_nil
+      @config.has_key?(:encrypt_pass).should_not be_true
+    end
+  end
+
+  describe 'encrypts' do
+    it 'all params with :encrypted' do
       Configliere::Crypter.should_receive(:encrypt).with('encrypt_me', 'pass').and_return('ok_encrypted')
       @config.send(:export).should == { :normal_param => 'normal', :encrypted_secret => 'ok_encrypted'}
     end
-    it 'gets encrypted params successfully' do
-      Configliere::Crypter.should_receive(:encrypt).with('encrypt_me', 'pass').and_return('ok_encrypted')
-      @config.send(:encrypted, @config[:secret]).should == 'ok_encrypted'
-    end
-    it 'fails if no pass is set' do
+
+    it 'fails unless encrypt_pass is set' do
       # create the config but don't set an encrypt_pass
       @config = Configliere::Param.new :secret => 'encrypt_me', :normal_param => 'normal'
+      @config.use :encrypted
       lambda{ @config.send(:encrypted, @config[:secret]) }.should raise_error('Missing encryption password!')
     end
   end
 
-  describe 'decrypting encryptable params' do
-    it 'decrypts all params marked encrypted' do
+  describe 'decrypts' do
+    it 'all params marked encrypted' do
       @config.delete   :secret
       @config.defaults :encrypted_secret => 'decrypt_me'
       Configliere::Crypter.should_receive(:decrypt).with('decrypt_me', 'pass').and_return('ok_decrypted')
@@ -54,7 +68,9 @@ describe "Configliere::Encrypted" do
     end
     it 'encrypts' do
       Configliere::Crypter.should_receive(:encrypt).and_return(@encrypted_str)
-      Configliere::ConfigFile.should_receive(:write_yaml_file).with('/fake/file', :normal_param=>"normal", :encrypted_secret => @encrypted_str)
+      FileUtils.stub(:mkdir_p)
+      File.should_receive(:open).and_yield([])
+      YAML.should_receive(:dump).with({ :normal_param => "normal", :encrypted_secret => @encrypted_str })
       @config.save! '/fake/file'
     end
     it 'decrypts' do
@@ -67,6 +83,22 @@ describe "Configliere::Encrypted" do
       @config.should == { :loaded_param => "loaded", :secret => 'decrypt_me', :normal_param => 'normal' }
     end
   end
+
+  describe '#resolve!' do
+    it 'calls super and returns self' do
+      Configliere::ParamParent.class_eval do def resolve!() dummy ; end ; end
+      @config.should_receive(:dummy)
+      @config.resolve!.should equal(@config)
+      Configliere::ParamParent.class_eval do def resolve!() self ; end ; end
+    end
+
+    it 'removes the encrypt_pass from sight' do
+      @config[:encrypt_pass] = 'hello'
+      @config.resolve!
+      @config.send(:instance_variable_get, "@encrypt_pass").should == 'hello'
+      @config[:encrypt_pass].should be_nil
+      @config.has_key?(:encrypt_pass).should_not be_true
+    end
+  end
+
 end
-
-
