e802facb8337547858a6cb2ebe16f70553af160a
Philip (flip) Kromer <flip@infochimps.org>
2012-06-30 15:20:18 -0500

    hacky improvement to the sub-command business

--- examples/joke.rb
+++ examples/joke.rb
@@ -8,11 +8,11 @@ Settings.define_command :q1, :description => "The first joke (question)"
 Settings.define_command :q2, :description => "The second joke (question)"
 Settings.define_command :q3, :description => "The third joke (question)"
 
-Settings.define_command :a1, :description => "The first joke (answer, all ages)"
-Settings.define_command :a2, :description => "The second joke (answer, PG-13)" do |cmd|
-  cmd.define :age_limit, :type => Integer,  :default => 12, :description => "minimum age to be able to enjoy joke 2"
+Settings.define_command :a1, :description => "The first joke (answer -- all ages)"
+Settings.define_command :a2, :description => "The second joke (answer -- PG-13, themes of death)" do |cmd|
+  cmd.define :age_limit, :type => Integer,  :default => 13, :description => "minimum age to be able to enjoy joke 2"
 end
-Settings.define_command :a3, :description => "The third joke (answer, R)" do |cmd|
+Settings.define_command :a3, :description => "The third joke (answer -- R, strong language)" do |cmd|
   cmd.define :age_limit, :type => Integer,  :default => 17, :description => "minimum age to be able to enjoy joke 3"
   cmd.define :bleep,     :type => :boolean, :default => false, :description => "if enabled, solecisms will be bowlerized"
 end
@@ -24,32 +24,39 @@ Settings.define :fake_id, :type => :boolean, :default => false, :description => 
 class Clown
 
   def q1
-    "what do you call a deer with no eyes?"
+    puts "what do you call a deer with no eyes?"
   end
 
   def a1
-    "no-eye deer"
+    q1
+    puts "no-eye deer"
   end
 
   def q2
-    "what do you call a dead deer with no eyes?"
+    puts "what do you call a dead deer with no eyes?"
   end
 
   def a2
-    "still no-eye deer"
+    q2
+    puts "still no-eye deer"
   end
 
   def q3
-    "what do you call a dead, castrated deer with no eyes?"
+    puts "what do you call a dead, castrated deer with no eyes?"
   end
 
   def a3
-    "still no-#{Settings[:bleep] ? 'bleeping' : 'fucking'} no-eye deer"
+    q3
+    puts "still no-#{Settings[:bleep] ? 'bleeping' : 'fucking'} no-eye deer"
   end
 
   def check_age_limit!
     return if not Settings[:age_limit]
-    if (Settings.age < Settings[:age_limit]) && (not Settings[:fake_id])
+    if Settings[:fake_id]
+      puts "ok but you didn't hear it from me...\n\n"
+      return true
+    end
+    if (Settings.age < Settings[:age_limit])
       warn "Sorry kid, you're too young for this joke. Try this again when you're older (or maybe ask for --help)"
       exit(1)
     end
@@ -77,4 +84,4 @@ if Settings.debug
 end
 
 
-puts Clown.new.run
+Clown.new.run
