64a1467f8ea13d060b686d8f6ed13dc76ce235b4
Travis Dempsey <dempsey.travis@gmail.com>
2013-08-21 18:15:34 -0500

    use subject block for testing over before block with instance variable reuse

--- spec/configliere/config_block_spec.rb
+++ spec/configliere/config_block_spec.rb
@@ -1,30 +1,29 @@
 require 'spec_helper'
 Configliere.use :config_block
 
-describe "Configliere::ConfigBlock" do
-  before do
-    @config = Configliere::Param.new :normal_param => 'normal'
-  end
+describe Configliere::ConfigBlock do
+
+  subject{ Configliere::Param.new :normal_param => 'normal' }
 
   describe 'resolving' do
     it 'runs blocks' do
       @block_watcher = 'watcher'
       # @block_watcher.should_receive(:fnord).with(@config)
       @block_watcher.should_receive(:fnord)
-      @config.finally{|arg| @block_watcher.fnord(arg) }
-      @config.resolve!
+      subject.finally{|arg| @block_watcher.fnord(arg) }
+      subject.resolve!
     end
     it 'resolves blocks last' do
       Configliere.use :config_block, :encrypted
-      @config.should_receive(:resolve_types!).ordered
-      @config.should_receive(:resolve_finally_blocks!).ordered
-      @config.resolve!
+      subject.should_receive(:resolve_types!).ordered
+      subject.should_receive(:resolve_finally_blocks!).ordered
+      subject.resolve!
     end
 
     it 'calls super and returns self' do
       Configliere::ParamParent.class_eval do def resolve!() dummy ; end ; end
-      @config.should_receive(:dummy)
-      @config.resolve!.should equal(@config)
+      subject.should_receive(:dummy)
+      subject.resolve!.should equal(subject)
       Configliere::ParamParent.class_eval do def resolve!() self ; end ; end
     end
   end
@@ -32,10 +31,9 @@ describe "Configliere::ConfigBlock" do
   describe '#validate!' do
     it 'calls super and returns self' do
       Configliere::ParamParent.class_eval do def validate!() dummy ; end ; end
-      @config.should_receive(:dummy)
-      @config.validate!.should equal(@config)
+      subject.should_receive(:dummy)
+      subject.validate!.should equal(subject)
       Configliere::ParamParent.class_eval do def validate!() self ; end ; end
     end
   end
-
 end
