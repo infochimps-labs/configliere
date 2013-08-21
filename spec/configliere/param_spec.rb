fda2f25e1d8d3ee28c5b1d2c9e1ebcc9479a05ae
Travis Dempsey <dempsey.travis@gmail.com>
2013-08-21 18:00:33 -0500

    use subject block for testing over before block with instance variable reuse

--- spec/configliere/param_spec.rb
+++ spec/configliere/param_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
-describe "Configliere::Param" do
-  before do
-    @config = Configliere::Param.new :hat => :cat, :basket => :lotion, :moon => { :man => :smiling }
-  end
+describe Configliere::Param do
+
+  subject{ described_class.new(:hat => :cat, :basket => :lotion, :moon => { :man => :smiling }) }
 
   describe 'calling #defaults' do
     it 'deep_merges new params' do
-      @config.defaults :basket => :tasket, :moon => { :cow => :jumping }
-      @config.should == { :hat => :cat, :basket => :tasket, :moon => { :man => :smiling, :cow => :jumping } }
+      subject.defaults :basket => :tasket, :moon => { :cow => :jumping }
+      subject.should == { :hat => :cat, :basket => :tasket, :moon => { :man => :smiling, :cow => :jumping } }
     end
+
     it 'returns self, to allow chaining' do
-      obj = @config.defaults(:basket => :ball)
-      obj.should equal(@config)
+      obj = subject.defaults(:basket => :ball)
+      obj.should equal(subject)
     end
   end
 
@@ -20,29 +20,31 @@ describe "Configliere::Param" do
     before do
       Configliere.should_receive(:use).with(:foobar)
     end
+
     it 'requires the corresponding library' do
-      obj = @config.use(:foobar)
+      obj = subject.use(:foobar)
     end
+
     it 'returns self, to allow chaining' do
-      obj = @config.use(:foobar)
-      obj.should equal(@config)
+      obj = subject.use(:foobar)
+      obj.should equal(subject)
     end
+
     it 'invokes the on_use handler' do
       Configliere::Param.on_use(:foobar) do
         method_on_config(:param)
       end
-      @config.should_receive(:method_on_config).with(:param)
-      @config.use(:foobar)
+      subject.should_receive(:method_on_config).with(:param)
+      subject.use(:foobar)
     end
   end
 
   describe '#resolve!' do
     it 'calls super and returns self' do
       Configliere::ParamParent.class_eval do def resolve!() dummy ; end ; end
-      @config.should_receive(:dummy)
-      @config.resolve!.should equal(@config)
+      subject.should_receive(:dummy)
+      subject.resolve!.should equal(subject)
       Configliere::ParamParent.class_eval do def resolve!() self ; end ; end
     end
   end
-
 end
